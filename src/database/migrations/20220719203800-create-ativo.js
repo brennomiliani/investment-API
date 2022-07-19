3630a6b80486c27d572e27af179f451921b1110a
Brenno Miliani <brennomiliani@hotmail.com>
2022-07-19 17:46:06 -0300

    feat: creating model and first migration for ativo

---
+++ src/database/migrations/20220719203800-create-ativo.js
@@ -0,0 +1,23 @@
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('Ativos', {
+      codAtivo: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER,
+      },
+      qtdAtivo: {
+        allowNull: false,
+        type: Sequelize.INTEGER,
+      },
+      nome: {
+        allowNull: false,
+        type: Sequelize.STRING,
+      },
+    });
+  },
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable('Ativos');
+  },
+};
---
+++ src/database/models/ativo.js
@@ -0,0 +1,15 @@
+module.exports = (sequelize, DataTypes) => {
+  const Ativo = sequelize.define('Ativo', {
+    codAtivo: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    qtdAtivo: DataTypes.INTEGER,
+    nome: DataTypes.STRING,
+  }, {
+    timestamps: false,
+    modelName: 'Ativo',
+  });
+  return Ativo;
+};
