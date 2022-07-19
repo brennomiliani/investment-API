c17a5e5257740052cc9b7d7289848ab1b8b1d2e5
Brenno Miliani <brennomiliani@hotmail.com>
2022-07-19 18:17:03 -0300

    feat: creating model and migration for ativos em custodia

---
+++ src/database/migrations/20220719210148-create-custodia-ativo.js
@@ -0,0 +1,35 @@
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('CustodiaAtivos', {
+      codAtivo: {
+        type: Sequelize.INTEGER,
+        references: {
+          model: 'Ativos',
+          key: 'codAtivo',
+        },
+        onUpdate: 'CASCADE',
+        onDelete: 'CASCADE',
+        primaryKey: true,
+      },
+      codCliente: {
+        type: Sequelize.INTEGER,
+        references: {
+          model: 'Clientes',
+          key: 'codCliente',
+        },
+        onUpdate: 'CASCADE',
+        onDelete: 'CASCADE',
+        primaryKey: true,
+      },
+      qtdAtivo: {
+        type: Sequelize.INTEGER,
+      },
+      valor: {
+        type: Sequelize.INTEGER,
+      },
+    });
+  },
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable('CustodiaAtivos');
+  },
+};
