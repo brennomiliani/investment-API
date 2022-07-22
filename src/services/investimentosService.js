c6c03882284fe4530d832148e0f35ca16ce66a1c
Brenno Miliani <brennomiliani@hotmail.com>
2022-07-21 23:32:46 -0300

    feat: created endpoint for POST /investimentos/vender

--- src/services/investimentosService.js
+++ src/services/investimentosService.js
@@ -4,7 +4,7 @@ const {
   getReasonPhrase,
   getStatusCode,
 } = require('http-status-codes');
-const { Cliente, Ativo, CustodiaAtivo } = require('../database/models');
+const { Cliente, CustodiaAtivo } = require('../database/models');
 const { getByAssets, getByClientAndAssets } = require('./ativosService');
 const { getCliente } = require('./contaService');
 
@@ -31,7 +31,7 @@ const comprar = async ({ codCliente, codAtivo, qtdAtivo }) => {
   if (clientAssets) {
     const newQtd = clientAssets.qtdAtivo + qtdAtivo;
     await CustodiaAtivo.update({
-      codCliente, codAtivo, qtdAtivo: newQtd, valorCompra: stock.preco,
+      qtdAtivo: newQtd, valorCompra: stock.preco,
     }, { where: { codCliente, codAtivo } });
   }
   if (!clientAssets) {
@@ -43,9 +43,32 @@ const comprar = async ({ codCliente, codAtivo, qtdAtivo }) => {
   return { status: StatusCodes.OK, message: 'Compra realizada!' };
 };
 
-const vender = async ({ codCliente }) => {
-  const cliente = await Cliente.findOne({ where: { codCliente } });
-  return cliente;
+const vender = async ({ codCliente, codAtivo, qtdAtivo }) => {
+  // verifica a quantidade a ser vendida é maior que a quantidade na carteira
+  const clientAssets = await getByClientAndAssets({ codCliente, codAtivo });
+  if (!clientAssets) return { status: StatusCodes.BAD_REQUEST, message: 'CodAtivo inválido' };
+  if (qtdAtivo > clientAssets.qtdAtivo) return { status: StatusCodes.BAD_REQUEST, message: 'Quantidade indisponível para venda' };
+
+  const [{ saldo }, { preco }] = await Promise.all([
+    await getCliente({ codCliente }),
+    await getByAssets({ codAtivo }),
+  ]);
+  const newSaldo = saldo + preco * qtdAtivo;
+
+  // atualiza saldo do cliente
+  await Cliente.update({ saldo: newSaldo }, {
+    where: {
+      codCliente,
+    },
+  });
+
+  // subtrai da custodia do ativo para o cliente
+  const newQtd = clientAssets.qtdAtivo - qtdAtivo;
+  await CustodiaAtivo.update({
+    qtdAtivo: newQtd,
+  }, { where: { codCliente, codAtivo } });
+
+  return { status: StatusCodes.OK, message: 'Compra realizada!' };
 };
 
 module.exports = {
