4a67668d40bb0c2517458cf9e7c76aebf92594c6
Brenno Miliani <brennomiliani@hotmail.com>
2022-07-23 15:43:16 -0300

    test: created the test for getAllAssetsAndQuantity

--- src/tests/services/ativosService.test.js
+++ src/tests/services/ativosService.test.js
@@ -4,7 +4,12 @@ const { expect } = require('chai');
 const { CustodiaAtivo, Ativo } = require('../../database/models');
 const ativosService = require('../../services/ativosService');
 const {
-  ativo, getByClientData, modelClientData, allAtivos,
+  ativo,
+  getByClientData,
+  modelClientData,
+  modelAtivos,
+  modelCustodiaAtivos,
+  getAllAssetsAndQuantityData,
 } = require('../mock/mockAtivos');
 
 describe('testing service layer for ativos', () => {
@@ -37,6 +42,24 @@ describe('testing service layer for ativos', () => {
       const stock = await ativosService.getByAssets({ codAtivo: 1 });
       expect(stock).to.be.an('object');
       expect(stock).to.be.equal(stock);
+    });
+  });
+
+  describe('finding all ativos and its quantities', () => {
+    before(() => {
+      sinon.stub(Ativo, 'findAll').resolves(modelAtivos);
+      sinon.stub(CustodiaAtivo, 'findAll').resolves(modelCustodiaAtivos);
+    });
+
+    after(() => {
+      Ativo.findAll.restore();
+      CustodiaAtivo.findAll.restore();
+    });
+
+    it('expect to return an array of all ativos its quantities', async () => {
+      const ativos = await ativosService.getAllAssetsAndQuantity();
+      expect(ativos).to.be.an('array');
+      expect(ativos).to.be.deep.equal(getAllAssetsAndQuantityData);
     });
   });
 });
