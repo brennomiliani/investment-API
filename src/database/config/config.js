3d21ea95dc289a813bb8a3724f3bd11e42e4ed6e
Brenno Miliani <brennomiliani@hotmail.com>
2022-07-24 17:43:24 -0300

    build: starting deploy

--- src/database/config/config.js
+++ src/database/config/config.js
@@ -6,20 +6,26 @@ module.exports = {
     password: process.env.MYSQL_PASSWORD,
     database: process.env.MYSQL_DB_NAME,
     host: process.env.MYSQL_HOST,
-    dialect: 'mysql',
+    dialect: process.env.DIALECT,
   },
   test: {
     username: process.env.MYSQL_USER,
     password: process.env.MYSQL_PASSWORD,
     database: process.env.MYSQL_DB_NAME,
     host: process.env.MYSQL_HOST,
-    dialect: 'mysql',
+    dialect: process.env.DIALECT,
   },
   production: {
     username: process.env.MYSQL_USER,
     password: process.env.MYSQL_PASSWORD,
     database: process.env.MYSQL_DB_NAME,
     host: process.env.MYSQL_HOST,
-    dialect: 'mysql',
+    dialect: process.env.DIALECT,
+    dialectOptions: {
+      ssl: {
+        require: true,
+        rejectUnauthorized: false,
+      },
+    },
   },
 };
